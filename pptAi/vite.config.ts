d9943f33df7ec30d421c6daa584e0991823115c8
Himanshu Anand <himanshuanand563@gmail.com>
2026-07-11 23:20:59 +0530

    feat: initialize project with TanStack Start, Vite, and Prisma configuration

--- pptAi/vite.config.ts
+++ pptAi/vite.config.ts
@@ -5,17 +5,14 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
   plugins: [
+    tsconfigPaths(),
     devtools(), 
     tailwindcss(), 
-    tanstackStart({
-      server: {
-        preset: 'vercel',
-      }
-    }), 
+    tanstackStart(), 
     viteReact()
   ],
 })
