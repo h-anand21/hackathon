591712a86799f04f839e607a3f02e4498ac31ed8
Himanshu Anand <himanshuanand563@gmail.com>
2026-07-11 23:41:38 +0530

    feat: initialize project with TanStack Start, Tailwind CSS, and Prisma monorepo configuration

--- pptAi/vite.config.ts
+++ pptAi/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,7 +13,8 @@ const config = defineConfig({
     tsconfigPaths(),
     devtools(), 
     tailwindcss(), 
-    tanstackStart(), 
+    tanstackStart(),
+    netlify(),
     viteReact()
   ],
 })
